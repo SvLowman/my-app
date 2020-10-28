43ef3ef53be5c90fd89fbdd85fb9947a035aece7
SvLowman <SvenLohmann@gmx.de>
2020-10-28 15:02:40 +0100

    Add favorite function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./global.css";
+import ImagePreview from "./components/ImagePreview";
+//import Button from "./components/Button";
+import { useState } from "react";
+import { getRandomImage } from "./api/getRandomImage.js";
+import "./App.css";
 
 function App() {
-  return <></>;
+  const [randomImage, setRandomImage] = useState(null);
+
+  async function handleClick() {
+    const randomImageResponse = await getRandomImage();
+    setRandomImage(randomImageResponse);
+  }
+
+  return (
+    <main>
+      <button onClick={() => handleClick()}>Gib mir ein Bild!</button>
+      {randomImage && (
+      <ImagePreview
+        id={randomImage.id}
+        src={randomImage.urls.regular}
+        alt={randomImage.alt_description}
+        author={randomImage.user.name}
+      />
+      )}
+    </main>
+  );
 }
 
 export default App;
